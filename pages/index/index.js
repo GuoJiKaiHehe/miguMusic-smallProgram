6f784fc063e1b69d955218955603bc6099301ec1
Z-LianYan <1371935318@qq.com>
2018-10-29 04:54:15 +0800

    miguMusic

---
+++ pages/index/index.js
@@ -0,0 +1,192 @@
+//index.js
+//获取应用实例
+const request = require('../../utils/request')
+const app = getApp()
+
+Page({
+  data: {
+    slideImgUrlData:[],
+    songListData:[],
+    newSongExpressData:[],
+    newDvdPutData:[],
+    newSongProduceData:[],
+    videoClData:[],
+    
+    isStopPlayA:true
+  
+  },
+  getSlideData () {//获取数据的方法
+    request({
+      url: 'http://m.music.migu.cn/migu/remoting/cms_list_tag',
+      data: {
+        pageSize: 10,
+        nid: 23831003,
+        pageNo: 0,
+        type: 2006
+      },
+      success: (res) => {
+        console.log(res.data.result.results)
+        this.setData({
+          slideImgUrlData: res.data.result.results
+        })
+      }
+    })
+  },
+
+  getSongList() {
+    request({
+      url: 'http://m.music.migu.cn/migu/remoting/cms_list_tag',
+      data: {
+        pageSize: 10,
+        nid: 23853969,
+        pageNo: 0,
+        type: 2006
+      },
+      success: (res) => {
+        console.log("我是songlist",res.data.result.results)
+        this.setData({
+          songListData: res.data.result.results
+        })
+      }
+    })
+  },
+  newSongExpress() {
+    request({
+      url: 'http://m.music.migu.cn/migu/remoting/cms_list_tag',
+      data: {
+        pageSize: 3,
+				nid: 23853978,
+				pageNo: 0
+      },
+      success: (res) => {
+        let results = res.data.result.results
+        results[0].songData.audio = 'audioA'
+        results[1].songData.audio = 'audioB'
+        results[2].songData.audio = 'audioC'
+
+        results[0].songData.isStopPlay = 'isStopPlayA'
+        results[1].songData.isStopPlay = 'isStopPlayB'
+        results[2].songData.isStopPlay = 'isStopPlayC'
+        console.log("我是newSongExpress", results)
+        this.setData({
+          newSongExpressData: results
+        })
+      }
+    })
+  },
+  newDvdPut() {
+    request({
+      url: 'http://m.music.migu.cn/migu/remoting/cms_list_tag',
+      data: {
+        pageSize: 10,
+				nid: 23854016,
+				pageNo: 0,
+				type: 2003
+      },
+      success: (res) => {
+        console.log("我是newDvdPut",res.data.result.results)
+        let result = res.data.result.results.slice(0,3)
+        this.setData({
+          newDvdPutData: result
+        })
+      }
+    })
+  },
+  newSongProduce() {
+    request({
+      url: 'http://m.music.migu.cn/migu/remoting/cms_list_tag',
+      data: {
+        pageSize: 10,
+				nid: 23854063,
+				pageNo: 0,
+				type: 2006
+      },
+      success: (res) => {
+        console.log("我是newSongProduce",res.data.result.results)
+        this.setData({
+          newSongProduceData: res.data.result.results
+        })
+      }
+    })
+  },
+  videoCl() {
+    request({
+      url: 'http://m.music.migu.cn/migu/remoting/cms_list_tag',
+      data: {
+        pageSize: 10,
+				nid: 24034103,
+				pageNo: 0,
+				type: 2038
+      },
+      success: (res) => {
+        console.log("我是videoCl",res.data.result.results)
+        this.setData({
+          videoClData: res.data.result.results
+        })
+      }
+    })
+  },
+
+  onLoad: function () {
+    this.getSlideData()
+    this.getSongList()
+    this.newSongExpress()
+    this.newDvdPut()
+    this.newSongProduce()
+    this.videoCl()
+  },
+
+  onReady:function(){
+    this.audioCtxA = wx.createAudioContext('audioA')
+    this.audioCtxB = wx.createAudioContext('audioB')
+    this.audioCtxC = wx.createAudioContext('audioC')
+  },
+
+  audioAPlay: function () {
+    this.setData({
+      isStopPlayA: false
+    })
+    this.audioCtxA.play()
+  },
+  audioAStop: function(){
+    this.setData({
+      isStopPlayA: true
+    })
+    this.audioCtxA.pause()
+  },
+
+  audioBPlay: function () {
+    this.setData({
+      isStopPlayA: false
+    })
+    this.audioCtxB.play()
+  },
+  audioBStop: function () {
+    this.setData({
+      isStopPlayA: true
+    })
+    this.audioCtxB.pause()
+  },
+
+  audioCPlay: function () {
+    this.setData({
+      isStopPlayA: false
+    })
+    this.audioCtxC.play()
+  },
+  audioCStop: function () {
+    this.setData({
+      isStopPlayA: true
+    })
+    this.audioCtxC.pause()
+  },
+  
+  
+  
+  
+  
+  
+  
+  
+
+})
---
+++ utils/request.js
@@ -0,0 +1,14 @@
+
+
+const request = ( {url ,data, success} ) => {
+    wx.request({
+        url: url, //仅为示例，并非真实的接口地址
+        data: data,
+        header: {
+            'content-type': 'application/json' // 默认值
+        },
+        success: success
+      })
+}
+
+module.exports = request
